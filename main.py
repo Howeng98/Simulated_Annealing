af51f5baa1ab354593210eed1791be3944134d8c
howeng98 <leonhwlee9@gmail.com>
2020-09-29 13:31:08 +0800

    update sumulated anneling result

--- main.py
+++ main.py
@@ -1,33 +1,63 @@
 from functions import func
-from random import randint
+from random import randint, uniform
+from math import exp
 
 
-def Acceptance(current_cost,new_cost,temperature):
-    delta = new_cost - current_cost
-    if  delta > 0:
-        return new_cost
-    else:
-        return exp(-delta/temperature)
+def Acceptance(delta,temperature):    
+    return exp(-(delta)/temperature)
+
+def generate_NextState(x1,x2,y1,y2,current_state_x,current_state_y):
+    next_state_x = randint(x1,x2)
+    next_state_y = randint(y1,y2)
+    while next_state_x == current_state_x and next_state_y == current_state_y:
+        next_state_x = randint(x1,x2)
+        next_state_y = randint(y1,y2)
+    return next_state_x,next_state_y 
+    
 
 def Simulated_Annealing(x1,x2,y1,y2):
     # Temperature define
-    T_Max = 100
-    T_Min = 0
+    T_Max = 10000
+    T_Min = 0.001
     T = T_Max
-    markov_step = 100
+    cooling_rate = 0.9999   
     
     # Initial State
     current_state_x = randint(x1,x2)
     current_state_y = randint(y1,y2)
     current_cost = func(current_state_x,current_state_y)
 
+    while T > T_Min:        
+        # generate next state
+        next_state_x,next_state_y = generate_NextState(x1,x2,y1,y2,current_state_x,current_state_y)
 
-    while T != T_Min:
-        for i in range(1,markov_step):
-            if T == 0:
-                return current
+        # calculate next cost
+        next_cost = func(next_state_x,next_state_y)
 
+        # calculate delta energy cost between current state and next state
+        delta = next_cost - current_cost
 
+        # if next state cost is better than current 
+        if(delta < 0):
+            current_state_x = next_state_x
+            current_state_y = next_state_y
+            current_cost = next_cost
+        # next state is worse than current one
+        else:
+            probability = Acceptance(delta,T)
+            threshold = uniform(0,1)
+        
+            # randomly give a chance to replace
+            if(threshold < probability):
+                current_state_x = next_state_x
+                current_state_y = next_state_y
+                current_cost = next_cost
+
+        # update Temperature
+        T = T * cooling_rate
+    
+    print('------ Simulated_Annealing ------')
+    print('X:{} Y:{} Z:{:.2f}'.format(current_state_x,current_state_y,current_cost))        
 
 def Brutal_Force(x1,x2,y1,y2):
     z = 0
@@ -38,17 +68,19 @@ def Brutal_Force(x1,x2,y1,y2):
                 x = i
                 y = j
                 z = result
-    print('X:{} Y:{}'.format(x,y))
-    print('Z:{:.2f}'.format(z))
-    
+    print('---------- Brutal_Force ---------')
+    print('X:{} Y:{} Z:{:.2f}'.format(x,y,z))
 
 def main():
+    # Read input data range
     input_data = []
     f = open('input.txt','r+')
     content = f.read()    
     content = content.split('\n')    
     input_data += content[0].split(',')
     input_data += content[1].split(',')    
+
+    # Calculate Minimum value of Z
     Brutal_Force(int(input_data[0]),int(input_data[1]),int(input_data[2]),int(input_data[3]))
     Simulated_Annealing(int(input_data[0]),int(input_data[1]),int(input_data[2]),int(input_data[3]))
 

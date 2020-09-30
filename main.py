9ed1df7848c88e9af9a138a7463661f5b5d0465d
howeng98 <leonhwlee9@gmail.com>
2020-09-30 14:36:10 +0800

    update some data visualization graph

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from functions import func
 from random import randint, uniform
 from math import exp
+import matplotlib.pyplot as plt
 
 
 def Acceptance(delta,temperature):    
@@ -14,21 +15,38 @@ def generate_NextState(x1,x2,y1,y2,current_state_x,current_state_y):
         next_state_y = randint(y1,y2)
     return next_state_x,next_state_y 
     
-    
-def Simulated_Annealing(x1,x2,y1,y2):
-    # Temperature definecurrent_cost
-    T_Max = 100000
+
+def Simulated_Annealing(x1,x2,y1,y2):    
+    # Temperature definecurrent_cost    
+    T_Max = 1000
     T_Min = 0.001
-    T = T_Max
-    cooling_rate = 0.99999
+    T = T_Max    
+    cooling_rate = 0.9999
+    cost_list = []
+    step_list = []
+    t_list = []
+    x_list = []
+    y_list = []
+    prob_list = []
+    counter = 0
+    probability = 0
     
     # Initial State
     current_state_x = randint(x1,x2)
     current_state_y = randint(y1,y2)
     current_cost = func(current_state_x,current_state_y)
+    cost_list.append(abs(current_cost))
+    step_list.append(counter)
+    t_list.append(T)
+    x_list.append(current_state_x)
+    y_list.append(current_state_y)
+    prob_list.append(0)
+    got_change_list = []
+    got_change_list.append(0)
+    counter = counter + 1
 
     while T > T_Min:        
-        # generate next state
+        # generate next state        
         next_state_x,next_state_y = generate_NextState(x1,x2,y1,y2,current_state_x,current_state_y)
 
         # calculate next cost
@@ -42,6 +60,8 @@ def Simulated_Annealing(x1,x2,y1,y2):
             current_state_x = next_state_x
             current_state_y = next_state_y
             current_cost = next_cost
+            got_change_list.append(0)
+            # cost_list.append(abs(current_cost))
         # next state is worse than current one
         else:
             probability = Acceptance(delta,T)
@@ -52,10 +72,113 @@ def Simulated_Annealing(x1,x2,y1,y2):
                 current_state_x = next_state_x
                 current_state_y = next_state_y
                 current_cost = next_cost
+                # cost_list.append(abs(current_cost))
+                got_change_list.append(1)
+            else:
+                got_change_list.append(0)
+                
 
         # Update Temperature
         T = T * cooling_rate
+
+        # plot graph - list
+        t_list.append(T)
+        step_list.append(counter)
+        cost_list.append(abs(current_cost))
+        x_list.append(current_state_x)
+        y_list.append(current_state_y)
+        prob_list.append(probability)
+        counter = counter + 1
     
+    x_list.sort()
+    y_list.sort()
+    # cost_list.sort()
+    plt.plot(t_list[::200],got_change_list[::200],'b-')
+    plt.xlabel('Temperature Variation')
+    plt.ylabel('Second Chance is Accepted')
+    plt.show()
+
+
+    ######################################################################################################
+    # T_Max = 10000
+    # T_Min = 0.001
+    # T = T_Max    
+    # cooling_rate = 0.99
+    # cost_list = []
+    # step_list = []
+    # t_list = []
+    # x_list = []
+    # y_list = []
+    # counter = 0
+    
+    # # Initial State
+    # current_state_x = randint(x1,x2)
+    # current_state_y = randint(y1,y2)
+    # current_cost = func(current_state_x,current_state_y)
+    # cost_list.append(abs(current_cost))
+    # step_list.append(counter)
+    # t_list.append(T)
+    # x_list.append(current_state_x)
+    # y_list.append(current_state_y)
+    # counter = counter + 1
+    # while T > T_Min:        
+    #     # generate next state        
+    #     next_state_x,next_state_y = generate_NextState(x1,x2,y1,y2,current_state_x,current_state_y)
+
+    #     # calculate next cost
+    #     next_cost = func(next_state_x,next_state_y)
+
+    #     # calculate delta energy cost between current state and next state
+    #     delta = next_cost - current_cost
+
+    #     # if next state cost is better than current 
+    #     if(delta < 0):
+    #         current_state_x = next_state_x
+    #         current_state_y = next_state_y
+    #         current_cost = next_cost
+    #         # cost_list.append(abs(current_cost))
+    #     # next state is worse than current one
+    #     else:
+    #         probability = Acceptance(delta,T)
+    #         threshold = uniform(0,1)
+        
+    #         # randomly give a chance to replace
+    #         if(threshold < probability):
+    #             current_state_x = next_state_x
+    #             current_state_y = next_state_y
+    #             current_cost = next_cost
+    #             # cost_list.append(abs(current_cost))
+                
+
+    #     # Update Temperature
+    #     T = T * 0.999
+
+    #     # plot graph - list
+    #     t_list.append(T)
+    #     step_list.append(counter)
+    #     cost_list.append(abs(current_cost))
+    #     x_list.append(current_state_x)
+    #     y_list.append(current_state_y)
+    #     counter = counter + 1
+    
+    # #for cost in cost_list:
+    # '''
+    # If plot like the below code,matplotlib will gonna overflow
+    # Actually we just no need to print so much data point,
+    # Try to print it out every 100 or even 1000 data
+    # # plt.plot(x_list,y_list,color='red')   
+    # # plt.plot(x_list[::100], y_list[::100])
+
+    # # mpl.rcParams['agg.path.chunksize'] = 10000
+    # '''
+    # # plt.plot(x_list,y_list,color='red')   
+    # x_list.sort()
+    # y_list.sort()
+    # # cost_list.sort()
+    # # plt.plot(step_list[::1], cost_list[::1],'b-')
+    # # plt.legend('T = 0.99999','T = 0.999')   
+    
+
     print('------ Simulated_Annealing ------')
     print('X:{} Y:{} Z:{:.2f}'.format(current_state_x,current_state_y,current_cost))        
 
